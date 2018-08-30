b0a1505becaff0ca8b2c6cdda0353d54dd6869a2
julietadevf <julieta.itumi@gmail.com>
2018-08-30 01:12:35 -0500

    Index con icono de explorador y fuentes definidas, 2.html con ddisplay funcional, icono de explorador y fuentes definidas

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-
-$(function() {
-  $('progress').each(function() {
-    var max = $(this).val();
-    $(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
-      });
-});
-
 let btnenviar1 = document.getElementById("btnenviar1")
 let nombre = document.getElementById("nombre")
 let email = document.getElementById("email")
 let psw = document.getElementById("psw")
-let formdatos = document.getElementById("formdatos")
+let btnsiguiente = document.getElementById("btnsiguiente")
+let btnguardar = document.getElementById("btnguardar")
 
 let usuario ={};
 
 const enviar1 = () => {
-    formdatos.style.display = "none"
+    formdatos.style.display = "none",
+    divingresos.style.display = "flex",
+    hola.style.display = "flex"
     usuario = {
         nombre: nombre.value,
         email: email.value,
         psw: psw.value,
     }
-    console.log(usuario)
+    rnombre.innerHTML = `¡Hola ${usuario.nombre}!`
+}
+console.log('El usuario: ', usuario )
+btnenviar1.addEventListener("click", enviar1)
+
+const siguiente = () => {
+    divingresos.style.display = "none";
+    divese.style.display = "flex";
 }
 
-console.log('El usuario: ', usuario )
+btnsiguiente.addEventListener("click",siguiente)
 
-btnenviar1.addEventListener("click", enviar1)
+const guardar = () => {
+    divese.style.display = "none";
+    rnombre.style.display = "none";
+
+}
+
+btnguardar.addEventListener("click",guardar)
