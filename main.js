4c42964881648a2627692896b1b513febe95a000
julietadevf <julieta.itumi@gmail.com>
2018-08-29 19:53:29 -0500

    Se crearon index y pagina 2 donde el formulario desaparece al crear el objeto usuario

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ let btnenviar1 = document.getElementById("btnenviar1")
 let nombre = document.getElementById("nombre")
 let email = document.getElementById("email")
 let psw = document.getElementById("psw")
+let formdatos = document.getElementById("formdatos")
 
 let usuario ={};
 
 const enviar1 = () => {
-    let usuario = {
+    formdatos.style.display = "none"
+    usuario = {
         nombre: nombre.value,
         email: email.value,
         psw: psw.value,
     }
-    console.log(usuario);
-    return usuario
+    console.log(usuario)
 }
+
+console.log('El usuario: ', usuario )
 
 btnenviar1.addEventListener("click", enviar1)
