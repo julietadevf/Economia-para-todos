774d17342e1b46422ba7be6319ef0a45ed12c78d
julietadevf <julieta.itumi@gmail.com>
2018-08-30 21:47:03 -0500

    Listo para recibir calculadora de ahorro de Karen

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let btnguardar = document.getElementById("btnguardar")
 let btnbajo = document.getElementById("btnbajo")
 let btnmedio = document.getElementById("btnmedio")
 let btnalto = document.getElementById("btnalto")
+let inputahorro = document.getElementById("%ahorro")
 
 
 let usuario ={};
@@ -35,8 +36,20 @@ btnsiguiente.addEventListener("click",siguiente)
 const guardar = () => {
     divp2.style.display = "none";
     divp3.style.display = "block";
-
-}
+    if (inputahorro.value >= 0 && inputahorro.value <=5) {
+        rbajo.style.display = "flex", 
+        rmedio.style.display = "none", 
+        ralto.style.display = "none"
+    } else if (inputahorro.value >= 5 && inputahorro.value <=15) {
+        rmedio.style.display = "flex",
+        rbajo.style.display = "none", 
+        ralto.style.display = "none"
+    } else if (inputahorro.value >= 15 && inputahorro.value <=100) {
+        ralto.style.display = "flex", 
+        rmedio.style.display = "none", 
+        rbajo.style.display = "none"
+    }
+    }
 
 btnguardar.addEventListener("click",guardar)
 
